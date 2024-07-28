1734c3bb2f8c4827daf1a5ceeb6aef4fac4465d0
Krystsina Aleinik <kristina.aleinik1996@gmail.com>
2024-07-28 20:07:38 +0200

    1st version

---
+++ ExtraVehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory
+{
+    internal class ExtraVehicle : IVehicle
+    {
+        public string? ExtraEngine {  get; set; }
+        public string? ExtraWheel { get; set; }
+        public string? ExtraSuspesion { get; set; }
+        public string? GetEngineType()
+        {
+            return ExtraEngine;
+        }
+        public string? GetWheelType()
+        { 
+            return ExtraWheel;
+        }
+        public string? GetSuspensionType()
+        { 
+            return ExtraSuspesion;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Abstract_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
