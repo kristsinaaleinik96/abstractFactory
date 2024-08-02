e3674b4eb27e68b017cb88cdd12024e1f7ab7d45
Krystsina Aleinik <kristina.aleinik1996@gmail.com>
2024-08-02 11:57:48 +0200

    add standart details

---
+++ IDetailsAbstractFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory
+{
+    internal interface IDetailsAbstractFactory
+    {
+        string CreateEngine();
+        string CreateWheel();
+        string CreateSuspension();
+    }
+}
